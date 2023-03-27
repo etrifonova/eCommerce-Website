83df62696ea5319d32511fcdc17302cf1472d770
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-03-27 13:13:37 +0300

    feat: add functions for cart

--- main.js
+++ main.js
@@ -3,6 +3,69 @@ let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
 
-cartIcon.onclick = function() {
+cartIcon.onclick = () => {
   cart.classList.add('active')
+}
+
+closeCart.onclick = () => {
+  cart.classList.remove('active');
+}
+
+// Cart
+
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready();
+}
+
+// Making function
+function ready() {
+  // Remove items from cart
+  var removeCartButtons = document.getElementsByClassName('cart-remove');
+  console.log(removeCartButtons);
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    let button = removeCartButtons[i];
+    button.addEventListener('click', removeCartItem);
+  }
+  // Quantity Changes
+  let quantityInputs = document.getElementsByClassName('cart-quantity');
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
+    input.addEventListener('change', quantityChanged);
+  }
+}
+
+// Remove items from cart 
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+// Quantity Changes
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+// Update total
+function updateTotal() {
+  let cartContent = document.getElementsByClassName('cart-content')[0];
+  let cartBoxes = cartContent.getElementsByClassName('cart-box');
+  let total = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName('cart-price')[0];
+    let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+    let price = parseFloat(priceElement.innerText.replace('₽', ''));
+    // The parseFloat() function parses a string argument and returns a floating point number.
+    let quantity = quantityElement.value;
+    total = total + price * quantity;
+    document.getElementsByClassName('total-price')[0].innerText = '₽' + total;
+    
+  }
+
 }
